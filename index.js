d323b95bc856b7bab68914b000d5d9f79cf5bab3
tuhin902 <tuhin902md@gmail.com>
2023-02-18 16:40:07 +0600

    fresher jobs data added

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 4000;
+
+// sunshine
+// NO5BygFoxPCaY0r6
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = "mongodb+srv://sunshine:NO5BygFoxPCaY0r6@cluster0.hesma7h.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const fresherJobsCollection = client.db('sunshine').collection('fresherJobs');
+
+        app.get('/fresherjobs', async (req, res) => {
+            const query = {};
+            const cursor = fresherJobsCollection.find(query);
+            const jobs = await cursor.toArray();
+            res.send(jobs);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.log(err));
+
 
 app.get('/', (req, res) => {
     res.send('sunshine server running');
